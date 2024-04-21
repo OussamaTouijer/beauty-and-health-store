28e210250305327bccb662252b964b55433ff7d2
OussamaTouijer <oussama5touijer@gmail.com>
2024-04-21 18:37:41 +0100

    1024

--- admin/categorise/listeCategorise.php
+++ admin/categorise/listeCategorise.php
@@ -30,7 +30,7 @@ $categoriesToShow = array_slice($categories, $startIndex, $categoriesPerPage);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Éclat & Vitalité (Admin)</title>
-    <link rel="stylesheet" href="../produits/stylesProduits.css">
+    <link rel="stylesheet" href="stylesCategorise.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
